df2fea0465181e0d75229f95a97952440b7ea5aa
3n1ac <eniac_z@icloude.com>
2026-08-07 14:44:22 +0800

    create astro mini blog

---
+++ src/utils/posts.ts
@@ -0,0 +1,71 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+
+const postsDirectory ="/Users/eniac/Documents/艾AA/2-Area/博客写作/Posts";
+
+
+export function getPosts(){
+
+    if(!fs.existsSync(postsDirectory)){
+        return [];
+    }
+
+
+    const folders =
+    fs.readdirSync(postsDirectory);
+
+
+    const posts = folders.map(folder=>{
+
+        const filePath =
+        path.join(
+            postsDirectory,
+            folder,
+            "index.md"
+        );
+
+
+        if(!fs.existsSync(filePath)){
+            return null;
+        }
+
+
+        const file =
+        fs.readFileSync(
+            filePath,
+            "utf-8"
+        );
+
+
+        const {data,content} =
+        matter(file);
+
+
+        return {
+
+            slug: folder,
+
+            ...data,
+
+            content
+
+        };
+
+    });
+
+
+    return posts
+        .filter(Boolean)
+        .filter(
+            post=>post.publish===true
+        )
+        .sort(
+            (a,b)=>
+            new Date(b.date).getTime()
+            -
+            new Date(a.date).getTime()
+        );
+
+}
